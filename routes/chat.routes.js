b0f997a653d4c3804885d2e1c46edec0651892fe
Zefanja Timorason <z.timorason@gmail.com>
2023-05-15 20:05:21 +0200

    chatroom route

---
+++ routes/chat.routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const chatRoom = require("../models/chatRoom.model");
+const Message = require("../models/Message.Model");
+
+//Create a chatroom route
+
+router.post("/chatroom", (req, res, next) => {
+  const { participants } = req.body;
+  chatRoom
+    .findOne({ participants: { $all: participants } })
+    .then((value) => {
+      if (value) {
+        res.json(value);
+      } else {
+        chatRoom.create({ participants }).then((value) => {
+          res.json(value);
+        });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
