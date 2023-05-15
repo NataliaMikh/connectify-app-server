9f1115848995f260d14ed0babb13773d12f276e6
Zefanja Timorason <z.timorason@gmail.com>
2023-05-15 21:08:12 +0200

    added the backend server

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 const app = require("./app");
+const { Server } = require("socket.io");
+const MessageModel = require("./models/Message.Model");
 
+let myServer = app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
+const io = new Server(myServer, {
+  cors: {
+    origin: "*",
+  },
+});
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("join_chat", (data) => {
+    socket.join(data);
+    console.log("User Joined Room: " + data);
+  });
+
+  socket.on("send_message", (data) => {
+    const {
+      content: { sender, message },
+      chatRoomId,
+    } = data;
+    let newMessage = {
+      sender: sender._id,
+      message: message,
+      chatRoomId: chatRoomId,
+    };
+
+    MessageModel.create(newMessage).then(async () => {
+      let allMessages = await MessageModel.find({
+        chatRoomId: chatId,
+      }).populate("sender");
+      socket.to(data.chatId).emit("receive_message", allMessages);
+    });
+  });
 });
